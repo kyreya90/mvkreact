f439b2a741b6550d0bf302f37cadf057e2a2f1eb
kyreya90 <kyreya90@ukr.net>
2023-04-25 21:32:37 +0300

    hm1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Simpsons from "./component/Simpsons/Simpsons";
+import {Characters} from "./component/Characters/Characters";
 
 const App = () => {
-  return (
-      <div>
-        
-      </div>
-  );
+    return (
+        <div>
+           {/*<Simpsons/>*/}
+            {<Characters/>}
+        </div>
+    );
 };
 
 export default App;
-
